0066110eba5e18cbc2963e681876ef9d403c248d
Josiah Smith <jcsmithf22@gmail.com>
2023-02-06 00:41:39 -0600

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import './app.css';
+</script>
+
+<div class="app">
+	<main class='flex flex-col gap-6 min-h-screen'>
+		<slot />
+	</main>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { fly } from "svelte/transition";
+	import { inview } from "svelte-inview";
+
+	let isInView: boolean[] = [];
+</script>
+
+<div
+	class="w-full bg-blue-600 text-center py-20"
+	use:inview={{ unobserveOnEnter: true }}
+	on:change={({ detail }) => {
+		isInView[0] = detail.inView;
+	}}
+>
+	{#if isInView[0]}
+		<h1 in:fly="{{y: -10, delay: 100}}" class='text-8xl font-bold mb-12'>Welcome to my portfolio</h1>
+		<p in:fly="{{y: -10, delay: 400 }}" class='text-2xl font-semibold'>Take a look at all the awesome products</p>
+	{/if}
+</div>
