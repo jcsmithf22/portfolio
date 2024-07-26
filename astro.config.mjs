bdf9a4822966f99b30ffa8bd722286d8772ba492
jcsmithf22 <jcsmithf22@gmail.com>
2024-07-26 13:58:44 -0500

    Remove old site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
   integrations: [mdx(), sitemap(), tailwind()],
 });
