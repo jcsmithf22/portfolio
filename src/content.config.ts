dc66d88eeed183a6010d70f1aabedf1123c4434c
Josiah Smith <jcsmithf22@gmail.com>
2026-01-06 13:48:06 -0600

    update to modern content collections

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
 import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
 const blog = defineCollection({
-	type: "content",
+	loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/content/blog" }),
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
@@ -11,7 +12,7 @@ const blog = defineCollection({
 });
 
 const work = defineCollection({
-	type: "content",
+	loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/content/work" }),
 	schema: z.object({
 		company: z.string(),
 		role: z.string(),
@@ -21,7 +22,10 @@ const work = defineCollection({
 });
 
 const projects = defineCollection({
-	type: "content",
+	loader: glob({
+		pattern: "**/[^_]*.{md,mdx}",
+		base: "./src/content/projects",
+	}),
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
